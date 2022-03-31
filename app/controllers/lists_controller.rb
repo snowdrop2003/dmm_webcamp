2c50ce76e88f691bbc07b6a11e68a5385c5bb851
EC2 Default User <ec2-user@ip-172-31-24-36.ec2.internal>
2022-03-31 02:11:29 +0000

    chapter2 finished

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,30 @@
 class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    # データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # データをデータベースに保存
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to '/top'
+
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
 end
--- db/migrate/20220329113810_create_lists.rb
+++ db/migrate/20220329113810_create_lists.rb
@@ -1,7 +1,6 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-
       t.timestamps
     end
   end
